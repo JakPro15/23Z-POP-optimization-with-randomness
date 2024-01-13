21e00efbf86f211a779f03d032a65e8cbd0b5f09
Jakub Proboszcz <jakub.proboszcz.stud@pw.edu.pl>
2024-01-13 13:11:48 +0100

    Changed eggholder limitation

--- functions.py
+++ functions.py
@@ -28,7 +28,7 @@ def himmelblau(x: Vector) -> float:
 
 def eggholder(x: Vector) -> float:
     if x[0] < -512 or x[0] > 512 or x[1] < -512 or x[1] > 512:
-        return np.inf
+        return sphere(x)
     return -1 * (x[1] + 47) * np.sin(np.sqrt(np.abs((x[0] / 2) + x[1] + 47))) - x[0] * np.sin(np.sqrt(np.abs(x[0] - x[1] - 47)))
 
 
