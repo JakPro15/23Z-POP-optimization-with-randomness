f1794aeb4b7d16199fe397ab88568ed61d6210a5
Paweł Kochański <pawilioxion@gmail.com>
2024-01-20 21:55:43 +0100

    added plot generation and updated experiments

--- experiments_de.py
+++ experiments_de.py
@@ -4,12 +4,13 @@ import random
 import statistics
 import sys
 from differential_evolution import differential_evolution
-from helpers import Vector, get_initial_population
+from helpers import Vector, get_initial_population, MAX_FUNCTION_CALLS
 
 
 def do_de_experiment(
     function: Callable[[Vector], float], sigma1: float, sigma2: float,
-    dimensions: int, population_size: int, differential_weight: float, crossover_threshold: float
+    dimensions: int, population_size: int, differential_weight: float, crossover_threshold: float,
+    max_iterations: int
 ) -> tuple[float, float, float, float]:
     results: list[float] = []
     for seed_value in range(25):
@@ -19,7 +20,7 @@ def do_de_experiment(
             population_size, dimensions)
         randomized_function = add_randomness(function, sigma1, sigma2)
         result, _ = differential_evolution(initial_population, randomized_function, differential_weight,
-                                           crossover_threshold, 1000)
+                                           crossover_threshold, max_iterations)
         result_fitness = function(result)
         results.append(result_fitness)
     average = statistics.mean(results)
@@ -46,18 +47,21 @@ if __name__ == "__main__":
     for sigma1 in [1, 10]:
         for sigma2 in [1, 10]:
             for dimensions in dimensions_options:
-                for population_size in [30, 50, 70]:
-                    for differential_weight in [0.5, 0.8, 1.1]:
-                        for crossover_threshold in [0.3, 0.5, 0.7]:
+                for population_size in [50, 70, 90]:
+                    for differential_weight in [0.3, 0.5, 0.7]:
+                        for crossover_threshold in [0.2, 0.4, 0.6]:
                             counter += 1
                             if counter <= file_length:
                                 continue
 
+                            max_iterations = (
+                                MAX_FUNCTION_CALLS - population_size) // (2 * population_size)
+
                             average, std_deviation, min_score, max_score = do_de_experiment(
-                                function, sigma1, sigma2, dimensions, population_size, differential_weight, crossover_threshold
+                                function, sigma1, sigma2, dimensions, population_size, differential_weight, crossover_threshold, max_iterations
                             )
                             with open(file_name, 'a+') as file:
                                 file.write(
                                     f"{function.__name__},{sigma1},{sigma2},{dimensions},{population_size},{differential_weight}," +
-                                    f"{crossover_threshold},{average},{std_deviation},{min_score},{max_score}\n"
+                                    f"{crossover_threshold},{max_iterations},{average},{std_deviation},{min_score},{max_score}\n"
                                 )
--- helpers.py
+++ helpers.py
@@ -3,6 +3,8 @@ from numpy.typing import NDArray
 
 Vector = NDArray[np.float64]
 
+MAX_FUNCTION_CALLS = 150_000
+
 
 def get_initial_population(size: int, dimensions: int) -> list[Vector]:
-    return [np.random.uniform(-1000, 1000, size=dimensions) for _ in range(size)]
+    return [np.random.uniform(-512, 512, size=dimensions) for _ in range(size)]
